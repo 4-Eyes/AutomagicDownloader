55449ce8e662dfa4cd79caf9484b30e28408819f
James Fairbairn <jamesaf112@gmail.com>
2016-01-21 16:53:35 +1300

    Minor fixes so that the information retrieved from the list is actually correct.

--- AutomagicDownloader/Tests/Program.cs
+++ AutomagicDownloader/Tests/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using MediaAPIs.IMDB;
 
 namespace Tests
@@ -16,7 +19,7 @@ namespace Tests
             movies.ForEach(item =>
             {
                 var imdbItem = item as Movie;
-                if (imdbItem != null)
+                if (imdbItem != null && imdbItem.RunTime != TimeSpan.Zero && imdbItem.Type != MediaType.MiniSeries && imdbItem.Type != MediaType.TVSeries && imdbItem.Type != MediaType.TVEpisode)
                 {
                     sadnessLevel = sadnessLevel.Add(imdbItem.RunTime);
                     totalIMDbRating += imdbItem.Rating;
@@ -26,7 +29,29 @@ namespace Tests
             });
             var aveRating = totalIMDbRating/itemCount;
             var aveUserRating = totalUserRating/itemCount;
+            var featureFilms =
+                movies.Where(item => (item as Movie).Type == MediaType.Feature).Select(thing => thing.Title).ToList();
+            featureFilms.Sort();
+            var duplicates = featureFilms.GroupBy(x => x)
+                .Where(group => group.Count() > 1)
+                .Select(group => group.Key).ToList();
+            WriteNamesToFile(featureFilms);
             Console.ReadKey();
+        }
+
+        private static void WriteNamesToFile(List<string> featureFilms)
+        {
+            if (File.Exists(@"C:\Users\James\Desktop\Movies.txt"))
+            {
+                File.WriteAllText(@"C:\Users\James\Desktop\Movies.txt", string.Empty);
+            }
+            using (var writer = new StreamWriter(@"C:\Users\James\Desktop\Movies.txt"))
+            {
+                foreach (var featureFilm in featureFilms)
+                {
+                    writer.Write(featureFilm + "\n");
+                }
+            }
         }
     }
 }
